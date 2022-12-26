2cf46d1cd2186a6640765f396fc4c5bffadff5b0
tanujadhope <tanuja_dhope@yahoo.com>
2022-12-26 16:40:39 +0530

    template file completed

---
+++ template.py
@@ -0,0 +1,41 @@
+import os 
+import logging
+from  pathlib  import Path
+ 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s:%(levelname)s]:%(message)s"
+)
+while True:
+    project_name=input("Enter your project name: ")
+    if project_name != '':
+       break
+logging.info(f"Creating project by name:{project_name}")
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    ".github/workflows/ci.yaml",
+    f"Mlproject/{project_name}/__init__.py",
+    f"test/__init__.py",
+    f"test/unit/__init__.py",
+    f"configs/config.yaml",
+    f"docs/index.md",
+    "init_setup.sh",
+    "requirements.txt",
+    "setup.py",
+    "setup.cfg",
+    "tox.ini",
+    "main.py",
+    "mkdocs.yaml"
+]
+for filepath in list_of_files:
+    filepath=Path(filepath)
+    filedir,filename=os.path.split(filepath)
+    if filedir!='':
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"creating a new directory :{filedir} for  path:{filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0): 
+        with  open(filepath,"w")as f:
+            pass
+            logging.info(f"creating a new file ::{filename} at path:{filepath}")
+    else:
+        logging.info(f"File is already present at:{filepath}")       
